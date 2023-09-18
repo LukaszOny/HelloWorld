f739f42f7bcb1239b689190677e4a48422b5ebc6
lonys <lonys@ONYSZEK>
2023-09-18 17:05:32 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿string imię = "Łukasz";
+Console.WriteLine("HelloWorld!");
+Console.WriteLine("Nazywam się Łukasz Onyszko i póki co nic nie kumam");
+Console.WriteLine("Mam nadzieję, że szybko to ogarnę...");
